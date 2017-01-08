3d965b8d0fd33a7d241f1fcbf4160aec14e4d333
Ben Zhang <benzhangniu@gmail.com>
2017-01-07 20:29:03 -0500

    initial server

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ AlexaAppServer.start({
   public_html: "public_html",
   app_dir: "apps",
   app_root: "/",
-  port: 8081
+  port: 8081,
+  preRequest: function(json,req,res) {
+    console.log("preRequest fired");
+  },
+  // Add a dummy attribute to the response
+  postRequest: function(json,req,res) {
+    json.dummy = "text";
+  }
 });
